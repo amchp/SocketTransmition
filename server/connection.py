d884fb4fee67e3d62ca8e4b787ac6d8c0d6be6d6
Alejandro Mc Ewen <alejandromcewen@Angelas-MacBook-Air-2.local>
2023-05-21 16:52:12 -0500

    Finish connection

--- server/connection.py
+++ server/connection.py
@@ -4,56 +4,60 @@ from dotenv import load_dotenv
 import os
 from types import FunctionType
 
+CHUNK_SIZE = 250 * 1000
 
-class Server():
+
+class Server:
     backlog = 2
 
-    def __init__(self)  -> None:
-        self.self_host = None
-        self.connection_port = None
-        self.socket = None
+    def __init__(self) -> None:
+        self.self_host: str = ""
+        self.connection_port: str = ""
         self.set_connection()
-    
+
     def multithread(func) -> FunctionType:
         def wrap(*args, **kwargs) -> None:
-            accept_thread = threading.Thread(target=func, args=[args])
+            accept_thread = threading.Thread(target=func, args=args)
             accept_thread.start()
             return
+
         return wrap
-    
+
     def set_connection(self) -> None:
-        self.socket = self.get_socket()
+        self.socket: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         load_dotenv()
         self.set_tcp_parameter()
         if self.self_host is None or self.connection_port == None:
             raise AssertionError(" Socket parameters not passed")
-    
+
     def set_tcp_parameter(self) -> None:
         self.self_host = os.getenv("SELFIP")
         self.connection_port = int(os.getenv("TCP_PORT"))
 
-    def get_socket(self) -> socket.socket:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        return s
-
     def start_server(self) -> None:
+        print("Start Server", flush=True)
         self.socket.bind((self.self_host, self.connection_port))
         self.socket.listen(Server.backlog)
         self.accept_connections()
 
-    @multithread
     def accept_connections(self) -> None:
         while True:
             client_socket, client_address = self.socket.accept()
             self.receive_messages(client_socket)
-    
+
     @multithread
-    def receive_messages(self, client_socket) -> None:
+    def receive_messages(self, client_socket: socket.socket) -> None:
         while True:
-            incoming_data = b"1"
-            data = b""
-            while incoming_data:
-                incoming_data = client_socket.recv(1024)
-                data += incoming_data
-            # Decompress data
-            print("Data:", data)
+            data: bytes = client_socket.recv(CHUNK_SIZE)
+            if data == b"":
+                continue
+            headers: list[bytes] = data.split()
+            print(headers[1])
+            data = b" ".join(data.split()[2:])
+            length: int = int(headers[0].decode())
+            file_name: str = headers[1].decode()
+            while len(data) < length:
+                data += client_socket.recv(CHUNK_SIZE)
+                print(length, len(data))
+            with open(f"./files/{file_name}", "bw") as file:
+                file.write(data)
