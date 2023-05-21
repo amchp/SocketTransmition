97b90f8f6327045c97b5fea6afedf17f20a40605
Alejandro McEwen <juanmcewen@Juans-Mac-mini.local>
2023-05-21 16:52:39 -0500

    Finished connection

--- client/connection.py
+++ client/connection.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 import os
-from socket import socket
+import socket
 from math import ceil
 
 CHUNK_SIZE = 250*1000
@@ -9,20 +9,23 @@ class Client:
     def __init__(self):
         pass
 
-    def start_client(self, target_host: str, connection_port: str, file_path : str) -> None:
+    def start_client(self, target_host: str, connection_port: int, file_path : str) -> None:
         client_socket = self.get_socket()
         client_socket.connect((target_host, connection_port))
         self.send_file(client_socket, file_path)
 
-    def get_socket(self) -> socket:  # Creates a BT socket
-        s = socket(socket.AF_INET, socket.SOCK_STREAM)
+    def get_socket(self) -> socket.socket:  # Creates a BT socket
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         return s
 
-    def send_file(self, client_socket : socket, file_path : str) -> None:
+    def send_file(self, client_socket : socket.socket, file_path : str) -> None:
         # Compress file
+        file_name = file_path.split("/")[-1]
         file_size = os.path.getsize(file_path)
+        headers = (f"{file_size} {file_name} ").encode()
+        client_socket.send(headers)
         chunks = ceil(file_size / CHUNK_SIZE)
         with open(file_path, "rb") as file:
             for _ in range(chunks):
-                client_socket.send(file.read(self.buffer))
+                client_socket.send(file.read(CHUNK_SIZE))
         client_socket.close()
--- client/main.py
+++ client/main.py
@@ -2,7 +2,7 @@ from connection import Client
 
 def main():
     client = Client()
-    client.send_file()
+    client.start_client("192.168.1.103", 3000, "/Users/juanmcewen/Desktop/Alejandro/Estudios/SistemasOperativos/SocketTransmition/client/test-music.mp3")
 
 if __name__ == "__main__":
     main()
